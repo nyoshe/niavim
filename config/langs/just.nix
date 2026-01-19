1056ff24c13a58ec2ecf053267a68b5daa38e214
Gabby Simpson <gabby@dotunwrap.dev>
2026-01-19 14:21:20 -0500

    fix: Disable just formatter

--- config/langs/just.nix
+++ config/langs/just.nix
@@ -1,18 +1,4 @@
-{ lib, pkgs, ... }:
 {
   lsp.servers.just.enable = true;
-  plugins = {
-    treesitter.settings.ensure_installed = [ "just" ];
-    conform-nvim.settings = {
-      formatters_by_ft.just = [ "just" ];
-      formatters.just = {
-        command = lib.getExe pkgs.just;
-        args = [
-          "--fmt"
-          "--stdin"
-        ];
-        stdin = true;
-      };
-    };
-  };
+  plugins.treesitter.settings.ensure_installed = [ "just" ];
 }
